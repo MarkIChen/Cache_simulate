df55c312c3271b7df56cde87603e0acd7ee17863
ChenICheng <c0919766098@yahoo.com.tw>
2019-06-10 12:15:36 +0800

    add four way

--- Cache_class.cpp
+++ Cache_class.cpp
@@ -2,6 +2,7 @@
 #include<iostream>
 #include "block.cpp"
 #include <vector>
+#include <deque>
 using namespace std;
 class Cache {
 
@@ -45,10 +46,8 @@ class Four_way : public Cache {
     public :
         unsigned int set_num, policy;
         // Block **all_set_ptr;
-        vector< vector<struct Block> > all_set;
-        
-
-        // vector<vector<struct Block> > all_set;
+        deque< deque<struct Block> > all_set;
+   
         
     Four_way(int block_size, int policy){
         this->policy = policy;
@@ -57,7 +56,7 @@ class Four_way : public Cache {
         // all_set = all_set
         // all_set_ptr = new Block*[set_num];
         for(int i=0; i< set_num; i++) {
-            vector<struct Block> row(0);
+            deque<struct Block> row(0);
             all_set.push_back(row);
         }
 
@@ -65,36 +64,67 @@ class Four_way : public Cache {
     }
 
     int read(int tag_dec, int index_dec){
-
         unsigned int set = index_dec / 4;
         cout<<"set = "<<set<<endl;
 
-        for(int  i = 0; i < all_set.at(set).size() ; i ++ ){
+        deque<struct Block>::iterator it = all_set.at(set).begin();
+        deque<struct Block>::iterator end = all_set.at(set).end();
 
-            unsigned int tag = all_set.at(set).at(i).tag;
-            unsigned int index = all_set.at(set).at(i).index;
-            bool valid = all_set.at(set).at(i).valid;
+        while(it != end ){
+
+            unsigned int tag = it->tag;
+            unsigned int index = it->index;
+            bool valid = it->valid;
 
             if(valid == 1 && tag_dec == tag && index_dec == index) { 
                 cout<<"hit"<<endl;
+                if(policy == 1 ){  //LRU
+                    // cout<<"improve privacy"<<endl<<"------"<<endl;
+                    addElement(set, index, tag);
+
+                    it = all_set.at(set).erase(it);
+                }
+
+
                 return -1;
             }
+            it++;
         }
-        //cannot find
-        if(all_set.at(set).size() < 4){
-            cout<<"now size in set = "<< all_set.at(set).size()<<endl;
-            cout<<"adding new element"<<endl<<"--------"<<endl;
+        
+        if(all_set.at(set).size() < 4){  //not four element yet
+            // cout<<"now size in set = "<< all_set.at(set).size()<<endl;
+            // cout<<"adding new element"<<endl<<"--------"<<endl;
+            
+            addElement(set, index_dec, tag_dec);
+            return -1;
 
-            struct Block element;
-            element.valid = 1;
-            element.index = index_dec;
-            element.tag = tag_dec;
+        } else{  //queue is Full
+           
+            // if(policy == 0){
+                // cout<<"this set is full, deleting tag_dec = "<< all_set.at(set).begin()->tag <<" index_dec = "<<all_set.at(set).begin()->index<<endl;
+                // cout<<"------"<<endl;
+                unsigned victim = all_set.at(set).begin()->tag;
+                all_set.at(set).pop_front();
+                addElement(set, index_dec, tag_dec);
+                
+                return victim;
+            // } else if(policy == 1){ //LRU
 
-            all_set.at(set).push_back(element);
+            // }
+
         }
 
 
         return 0 ;
+    }
+
+    void addElement(int set, int index_dec, int tag_dec){
+        struct Block element;
+        element.valid = 1;
+        element.index = index_dec;
+        element.tag = tag_dec;
+
+        all_set.at(set).push_back(element);
     }
 };
 
