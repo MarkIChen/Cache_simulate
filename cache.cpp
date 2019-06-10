347e9e1587bb9d248db04427a25b873536b527e4
ChenICheng <c0919766098@yahoo.com.tw>
2019-06-10 20:50:36 +0800

    mod

--- cache.cpp
+++ cache.cpp
@@ -15,8 +15,8 @@ int main(){
     
     long x;
 
-    ifstream t1_file("trace1_four.txt");
-    ofstream t1_out_file("trace1_test.txt");
+    ifstream t1_file("trace1.txt");
+    ofstream t1_out_file("trace.out");
 
     if(t1_file.is_open()){
         getline(t1_file, line);
